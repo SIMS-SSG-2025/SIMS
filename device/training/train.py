dbf25f0c64203085ccb4711f191b978f2d3277ec
Alfbom27 <alst2104@student.miun.se>
2025-09-19 11:35:31 +0200

    created training script

--- device/training/train.py
+++ device/training/train.py
@@ -1,2 +1,17 @@
 import torch
+import argparse
+import yaml
+import os
 
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--config",
+    type=str,
+    required=True,
+    help="Path to YAML config file",
+)
+
+args = parser.parse_args()
+print(args.config)
+
+# load training hyperparams from config.yaml
