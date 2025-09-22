4a6753b939dc1ca6451397555dca51412081defc
mehar <mehariweldeab1@gmail.com>
2025-09-22 14:47:17 +0200

    Change the database structure

--- backend/db/init_db.py
+++ backend/db/init_db.py
@@ -5,12 +5,15 @@ sqlconn = sqlite3.connect("events.db")
 cursor = sqlconn.cursor()
 timeslot = datetime.datetime.now()
 
-cursor.execute("""CREATE TABLE IF NOT EXISTS events ( id INTEGER PRIMARY KEY AUTOINCREMENT,
-               type TEXT NOT NULL,
+cursor.execute("""CREATE TABLE IF NOT EXISTS events (event_id INTEGER PRIMARY KEY AUTOINCREMENT,
+object_id INTEGER NOT NULL,zone_id INTEGER NOT NULL,
 time TEXT NOT NULL,
-has_helmet BOOLEAN,
-has_vest BOOLEAN, zone TEXT)""")
+type TEXT NOT NULL,ppe TEXT NOT NULL,location TEXT NOT NULL,
+FOREIGN KEY (zone_id) REFERENCES zones (zone_id))""")
 
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS zones (zone_id INTEGER PRIMARY KEY, coords TEXT NOT NULL, name TEXT NOT NULL)""")
+cursor.execute("""CREATE TABLE IF NOT EXISTS object (object_id INTEGER PRIMARY KEY ,type TEXT NOT NULL)""")
 
 sqlconn.commit()
 sqlconn.close()
